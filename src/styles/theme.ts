e46ffa7a9608d2ffbbc87818921a44a208c93656
josephdsbr <josephdsbr@gmail.com>
2021-01-02 15:51:21 -0300

    instalando styled components e tema global

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+    colors: {
+      background: '#53B175',
+      text: '#0d0c22',
+      primary: '#2B7F6E',
+      secondary: '#fff',
+      input: '#0d0c22',
+      scroll: '#2B7F6E',
+      scrollBackground: '#fff',
+      title: '#2B7F6E'
+    },
+    fonts: {
+      borderRadius: '8px',
+      inputPadding: '12px'
+    }
+  }
+  
+  export default theme
+  
