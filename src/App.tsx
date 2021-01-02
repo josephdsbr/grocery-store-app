2c77661e90941329eb116af1a97bb574b1e7f985
josephdsbr <josephdsbr@gmail.com>
2021-01-02 15:41:12 -0300

    startando o projeto e adicionando typescript

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>
+        Olá Mundo
+      </Text>
+    </View>
+  )
+}
+
+export default App;
